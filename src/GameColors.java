92cb43c4ac2b5b1a49edc6db363300d5ed365f6d
Rozenman100 <itamar.rozenman@gmail.com>
2024-12-01 13:14:26 +0200

    Delete src directory

--- src/GameColors.java
+++
@@ -1,7 +0,0 @@
-import java.awt.Color;
-
-public class GameColors {
-	public static final String colorsStringArr[] = { "שמחה", "רגע", "עצב", "חזקה", "פחד", "כעס" };
-	public static final Color  colorsArr[] = { Color.orange, Color.pink, Color.gray, Color.green, new Color(102, 0, 153),
-			Color.red };
-}
